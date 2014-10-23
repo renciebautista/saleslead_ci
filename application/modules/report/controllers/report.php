59b2cf8dec9950f63ce4a69da2cb904487352eb3
rencie bautista <rencie.bautista@yahoo.com>
2014-10-23 11:51:30 +0800

    update .htaccess

--- application/modules/report/controllers/report.php
+++ application/modules/report/controllers/report.php
@@ -4,6 +4,7 @@ class Report extends MY_Controller {
 
 	function __construct(){
 		parent::__construct();
+		$this->load->model('project/Project_detail_model');
 	}
 
 	public function index()
@@ -14,7 +15,7 @@ class Report extends MY_Controller {
 	public function callreport(){
 		$this->data['start_date'] = $this->input->get('start_date');
 		$this->data['end_date'] = $this->input->get('end_date');
-		$this->data['callreports'] = array();
+		$this->data['callreports'] = $this->Project_detail_model->
 		$this->layout->view('report/callreport',$this->data);
 	}
 
